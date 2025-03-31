54053f508d9e82ca492bb6a2b262c044af08f4ab
hanna-uler <hannauler@gmail.com>
2025-03-31 14:16:29 -0400

    create const for apiKey

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 export default async function getPics(queryWords, pageNum) {
     axios.defaults.baseURL = "https://pixabay.com";
     const pictures = await axios.get("/api/", {
         params: {
-            key: import.meta.env.VITE_API_KEY,
+            key: API_KEY,
             q: queryWords,
             image_type: "photo",
             orientation: "horizontal",
